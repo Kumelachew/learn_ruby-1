fa9bb7a8c14f72d6cb95487fb0a275ffd1600905
Kumelachew Abera <kumelachew.abera@gmail.com>
2016-12-26 18:49:45 -0500

    completed exercise hello,temperature, and started calculator.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(f_input)
+  temp_in_c = (f_input - 32) * (5.0/9.0)
+end
+
+def ctof(c_input)
+  temp_in_f = ((9.0/5.0) * c_input) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
-#write your code here
+# write your code here
+def add(first_number, second_number)
+  first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  first_number - second_number
+end
+
+def sum(input)
+  if input.to_a.empty? == true
+    result = 0
+  else
+    input.each do |value|
+      result = result.to_i + value.to_i
+    end
+  end
+  result.to_i
+end
+
+def multiply(input)
+  if input.empty? == true
+    result = 0
+  else
+    result = input.inject(:*)
+  end
+  result
+end
+
+def power(number, power)
+  number ** power
+end
